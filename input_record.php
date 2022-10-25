c52d8974d53675947cc63ba34212179a9bfbf477
beener7 <104628675+beener7@users.noreply.github.com>
2022-10-25 19:03:24 +0900

    Update input_record.php

--- input_record.php
+++ input_record.php
@@ -3,8 +3,22 @@
 <head>
 	<meta charset="utf-8">
 	<title>SignUp</title>
+<style>
 
-	<link rel="stylesheet" href="./style.css" type="text/css">
+.field {
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 10px;
+}
+input[type="submit"]{
+  min-width: 150px;
+  min-height: 50px;
+  margin: 20px 0;
+}
+
+</style>Yisunsin
+	
+<link rel="stylesheet" href="./style.css" type="text/css">
 </head>
 
 <body>
@@ -18,28 +32,32 @@
 			<input type="file" name="avatar" enctype="multipart/form-data">
 		</div>
 		<div class="field">
-			<label>Username</label>
+			<label>순서</label>
+			<input type="int" name="number" placeholder="number">
+		</div>
+		<div class="field">
+			<label>아이디</label>
 			<input type="text" name="username" placeholder="Username">
 		</div>
 		<div class="field">
-			<label>Password</label>
+			<label>비밀번호</label>
 			<input type="password" name="password" placeholder="Password">
 		</div>
 		<div class="field">
-			<label>Email</label>
+			<label>이메일</label>
 			<input type="email" name="email" placeholder="Email">
 		</div>
 		<div class="field">
-			<label>About Me</label>
+			<label>나를 소개하는 말</label>
 			<textarea name="introduce" placeholder="About Me"></textarea>
 		</div>
 		<div class="field">
-			<label>Website</label>
+			<label>내가 만든 웹사이트 </label>
 			<input type="text" name="site" placeholder="Your website">
 		</div>
 		<div class="field">
-			<label>Field</label>
-			<select name="field">
+			<label>직업</label>
+			<select name="field" required>
 				<option value="developer">Developer</option>
 				<option value="design">Design</option>
 				<option value="artist">Artist</option>
@@ -53,4 +71,3 @@
 	</form>
 </body>
 </html>
-
